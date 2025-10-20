b71cd1566da1eafbae3395e5f25ee4e647db870a
davide-dascanio <davidedascanio01@gmail.com>
2025-10-21 00:40:54 +0200

    Modifica del primo Homework-XHTML-CSS per la realizzazione del secondo Homework-PHP-MySQL
    - modifica delle pagine collegamento passando da risorse statiche a dinamiche (sono tutte .php)
     - aggiunta di un file header menu1.php che verrà require da ogni pagina collegamento per la costruzione della barra navigatrice,
       che in questo nuovo commit è stata modificata
     - aggiunta nel tag <head> di ogni pagina del progetto del link alla libreria di icone

---
+++ file/menu1.php
@@ -0,0 +1,27 @@
+<div class="nav">
+    <ul>
+        <li><a class="active" href="../../index.html">HOME</a></li>
+        <li class="posizione">
+            <a href="javascript:void(0)" style="cursor:pointer" onclick="apriNavFull()"> VEDI GLI ALTRI MONUMENTI </a>
+        </li>
+        <li> 
+            <a href="https://www.google.com/"><header><i class="fas fa-user"></i> ACCEDI/REGISTRATI </header></a>
+        </li>
+        <li>
+            <a href="https://www.google.com/"><header><i class="fas fa-shopping-cart"></i> VAI AL CARRELLO </header></a>
+        </li>
+    </ul>
+</div>
+<div id="navigatore" class="sovrapposizione">
+    <a href="javascript:void(0)" class="chiusuraNav" onclick="chiudiNavFull()">&times;</a>
+    <div class="sovrapposizione-cont">
+        <a href="../../index.html">HOME</a>
+        <a href="../collegamento_1/pagina1.php">Grande Muraglia Cinese, Cina </a>
+        <a href="../collegamento_2/pagina2.php">Petra, Giordania </a>
+        <a href="../collegamento_3/pagina3.php">Cristo Redentore, Rio de Janeiro </a>
+        <a href="../collegamento_4/pagina4.php">Machu Picchu, Cusco - Per&ugrave; </a>
+        <a href="../collegamento_5/pagina5.php">Chich&eacute;n Itz&aacute;, Yucat&agrave;n - Messico </a>
+        <a href="../collegamento_6/pagina6.php">Colosseo, Roma - Italia </a>
+        <a href="../collegamento_7/pagina7.php">Taj Mahal, Agra - India </a>
+    </div>
+</div>
